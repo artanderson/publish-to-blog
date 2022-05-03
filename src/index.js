9d5aab7248299ba29c2cb087f4c42b9dfff25851
artanderson <arthuranders@gmail.com>
2022-05-03 10:20:36 -0700

    switch from ESM to CJS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import core from '@actions/core';
-import * as git from '@actions/github';
-import matter from 'gray-matter';
-import { promises as fs } from 'fs';
-import fetch from "node-fetch";
-import NodeVault from 'node-vault';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
+const matter = require('gray-matter');
+const fetch = require('node-fetch');
+const core = require('@actions/core');
+const git = require('@actions/github');
+const fs = require('fs');
 
 const mediumPost = async (authToken, pubID, content, title, slug, tags) => {
     const article = {
@@ -95,7 +91,7 @@ const main = async () => {
             return 0;
         }
 
-        const vault = new NodeVault({
+        const vault = require('node-vault')({
             apiVersion: "v1",
             endpoint: endpoint,
         });
@@ -113,14 +109,14 @@ const main = async () => {
 
         for(let i = 0; i < mdFiles.length; i++){
             let fileExists = true;
-            fs.access(`./${mdFiles[i].filename}`, (err) => {
+            await fs.promises.access(`./${mdFiles[i].filename}`, (err) => {
                 if(err){
                     console.log('File does not exist');
                     fileExists = false;
                 }
             });
             if(fileExists){
-                let file = await fs.readFile(`./${mdFiles[i].filename}`, 'utf8');           
+                let file = await fs.promises.readFile(`./${mdFiles[i].filename}`, 'utf8');           
                 let article = matter(file);
 
                 let { author } = await vault.read(`blog-publish/data/${article.data.authors}`);
